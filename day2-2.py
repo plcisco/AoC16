8ee474d62895c849ea7d31b34962b5e7da99cd38
plcisco <pleys@cisco.com>
2016-12-02 22:05:53 +0100

    Day2 Puzzle2

---
+++ day2-2.py
@@ -0,0 +1,24 @@
+
+file = open("d2-1-input.txt", "r")
+instructions = file.readlines()
+x=1
+y=3
+layout = [["0","0","0","0","0","0","0"],
+		  ["0","0","0","1","0","0","0"],
+		  ["0","0","2","3","4","0","0"],
+		  ["0","5","6","7","8","9","0"],
+		  ["0","0","A","B","C","0","0"],
+		  ["0","0","0","D","0","0","0"],
+		  ["0","0","0","0","0","0","0"]]
+
+for instruction in instructions:
+	for i in range(0, len(instruction)):
+		if (instruction[i]=="L") & (layout[y][x-1]<>"0"):
+			x-=1
+		elif (instruction[i]=="R") & (layout[y][x+1]<>"0"):
+			x+=1
+		elif (instruction[i]=="D") & (layout[y+1][x]<>"0"):
+			y+=1
+		elif (instruction[i]=="U") & (layout[y-1][x]<>"0"):
+			y-=1
+	print layout[y][x]
