7a2d71b494f2c27d61ca7b886cabfc77f3ae5650
plcisco <pleys@cisco.com>
2016-12-02 22:19:34 +0100

    Day2 Puzzle2

--- day2-2.py
+++ day2-2.py
@@ -1,18 +1,18 @@
-instructions = open("d2-1-input.txt", "r").readlines()
+instructions = open('d2-1-input.txt', 'r').readlines()
 x=1
 y=3
-layout = [["0","0","0","0","0","0","0"],
-		  ["0","0","0","1","0","0","0"],
-		  ["0","0","2","3","4","0","0"],
-		  ["0","5","6","7","8","9","0"],
-		  ["0","0","A","B","C","0","0"],
-		  ["0","0","0","D","0","0","0"],
-		  ["0","0","0","0","0","0","0"]]
+layout = [['0','0','0','0','0','0','0'],
+		  ['0','0','0','1','0','0','0'],
+		  ['0','0','2','3','4','0','0'],
+		  ['0','5','6','7','8','9','0'],
+		  ['0','0','A','B','C','0','0'],
+		  ['0','0','0','D','0','0','0'],
+		  ['0','0','0','0','0','0','0']]
 
 for instruction in instructions:
 	for i in range(0, len(instruction)):
-		if (instruction[i]=="L") & (layout[y][x-1]<>"0"): x-=1
-		elif (instruction[i]=="R") & (layout[y][x+1]<>"0"): x+=1
-		elif (instruction[i]=="D") & (layout[y+1][x]<>"0"):	y+=1
-		elif (instruction[i]=="U") & (layout[y-1][x]<>"0"): y-=1
+		if (instruction[i]=='L') & (layout[y][x-1]<>'0'): x-=1
+		elif (instruction[i]=='R') & (layout[y][x+1]<>'0'): x+=1
+		elif (instruction[i]=='D') & (layout[y+1][x]<>'0'):	y+=1
+		elif (instruction[i]=='U') & (layout[y-1][x]<>'0'): y-=1
 	print layout[y][x]
